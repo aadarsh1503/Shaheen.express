fbf92cee7fcd74a2411d6db29dbdfb8717c0c739
aadarsh1503 <aadarshchauhan35@gmail.com>
2024-12-03 19:33:27 +0530

    dfone

--- client/src/components/Hero/Testimonials.jsx
+++ client/src/components/Hero/Testimonials.jsx
@@ -1,32 +1,42 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import i1 from "./i1.avif"
+import i100jpg from "./i100jpg.jpg"
+import i101 from "./i101.jpg"
+import i102 from "./i102.jpg"
 
 const Testimonials = () => {
   const testimonials = [
     {
       id: 1,
-      name: "John Doe",
-      text: "Seamless Global Shipping We've been using their services for years, and they consistently exceed our expectations. Their seamless global shipping solutions have streamlined our supply chain and reduced costs. We highly recommend them to businesses of all sizes.",
-      image: "https://images01.nicepage.com/a1389d7bc73adea1e1c1fb7e/f625e3501ea153e0acad867f/pexels-photo-9668543.jpeg",
+      name: "Sarah T.",
+      Designation:"Operations Manager,Global E-Commerce Brand",
+      text: "Partnering with Shaheen Express has been a game-changer for our international operations. Their seamless integration of real-time tracking, fast delivery times, and reliable customer service has significantly boosted our customer satisfaction. We can now ship to multiple regions without worrying about delays or issues. The team is always proactive in addressing challenges, and their innovative solutions have helped us scale our business more efficiently. We couldn't ask for a more dependable logistics partner.",
+      image: i1,
     },
     {
       id: 2,
-      name: "Jane Smith",
-      text: "Personalized Service and Expertise Their personalized service and expertise in international logistics are second to none. They took the time to understand our unique needs and provided tailored solutions. Their team is always available to answer our questions and provide updates, giving us peace of mind.",
-      image: "https://plus.unsplash.com/premium_photo-1672373830660-4655ca9de6c3?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWFsZSUyMG9mZmljZXxlbnwwfHwwfHx8MA%3D%3D",
+      name: "John M",
+      Designation:"Supply Chain Director, Leading Manufacturing Company",
+      text: "As a global manufacturer, timely and cost-effective delivery is critical to our production schedules. We've worked with Shaheen Express for over three years, and they’ve consistently exceeded our expectations. From handling complex customs processes to managing time-sensitive shipments. They communicate transparently, providing updates and offering solutions when unexpected issues arise. Their ability to adapt to our evolving needs has made them an essential part of our supply chain.",
+      image: i100jpg,
     },
     {
-      id: 3,
-      name: "Alex Johnson",
-      text: "Reliable and Efficient Partner Their team was incredibly responsive and efficient. They handled our complex international shipment with ease, ensuring timely delivery to our clients. We're impressed with their commitment to customer satisfaction and their ability to navigate global logistics challenges.",
-      image: "",
+      id: 3, 
+      name: "Lisa D.",
+      Designation:"Logistics Coordinator, Nationwide Retail Chain",
+      text: "Shaheen Express has completely transformed our distribution network. Their team took the time to understand our business and implemented a tailored logistics solution that maximized efficiency and reduced our costs. We’ve seen a drastic reduction in shipping errors, and our inventory management has improved thanks to their real-time tracking technology. Their dedication to meeting deadlines and their ability to handle seasonal spikes in demand has made them an indispensable part of our retail operations.",
+      image: i102,
     },
     {
       id: 4,
-      name: "Emily Davis",
-      text: "Efficient and Transparent Logistics Solutions Their efficiency and transparency have set a new standard for us. From managing documents to on-time deliveries, they’ve ensured a smooth experience every step of the way. Their proactive approach has significantly improved our operations.",
-      image: "https://media.istockphoto.com/id/2003613572/photo/smiling-bearded-man-in-glasses-at-a-casual-business-meeting.webp?a=1&b=1&s=612x612&w=0&k=20&c=E6AxIWxGEEqdXFxRCpWM77mFqrv19U2gV11mCnljs3g=",
-    },
+      Designation:"Regional Logistics Manager, International Freight Company",
+      name: "Michael Green",
+      text: "Our e-commerce platform relies on precise and dependable logistics, and Shaheen Express has consistently delivered. Their ability to handle large-scale shipments with accuracy and care has helped us maintain our reputation for timely deliveries. The team's commitment to providing personalized service, coupled with their innovative tracking tools, ensures that our customers are always informed. Their collaborative approach and problem-solving mindset have been crucial in navigating the complexities of international shipping.",
+      image: i101,
+    }
+    
+   
   ];
 
   const [current, setCurrent] = useState(0);
@@ -56,7 +66,7 @@ const Testimonials = () => {
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
-      transition={{ duration: 0.6, ease: "easeOut" }}
+      transition={{ duration: 1, ease: "easeOut" }}
       className="bg-gray-100 py-20"
     >
       <div className="lg:text-sm text-2xl text-dgreen text-center font-bold max-w-5xl mx-auto mb-6">
@@ -82,13 +92,13 @@ const Testimonials = () => {
           {reorderedTestimonials.map((testimonial, index) => (
             <motion.div
               key={testimonial.id}
-              className={`relative transition-all duration-1000 ease-in-out flex flex-col items-center bg-white shadow-md p-6 rounded-lg
+              className={`relative transition-all duration-1000 ease-in-out flex flex-col items-center font-poppins bg-white shadow-md p-3 rounded-lg
                 ${index === 1
-                  ? "scale-110 opacity-100 h-[420px] w-[332px] mb-9 transform shadow-xl transition-transform"
+                  ? "scale-110 opacity-100 h-[540px] w-[332px] mb-9 transform shadow-xl transition-transform"
                   : "scale-90 opacity-60 w-64 bg-gray-300 transform transition-transform"}`}
               initial={{ scale: 0.8, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
-              transition={{ duration: 0.6, ease: "easeOut", delay: index * 0.2 }}
+              transition={{ duration: 1, ease: "easeOut", delay: index * 0.2 }}
             >
               <div
                 className={`absolute -top-10 flex justify-center items-center w-24 h-24 rounded-full overflow-hidden border-4 ${index === 1 ? "border-lightgreen" : "border-gray-200"}`}
@@ -100,8 +110,9 @@ const Testimonials = () => {
                 />
               </div>
               <div className="text-5xl text-dgreen mt-4 lg:mt-10 ml-0 lg:-ml-56">“</div>
-              <p className="text-dgreen font-bold mb-1">{testimonial.name}</p>
-              <p className="text-gray-600 max-w-7xl text-center">
+              <p className="text-dgreen font-bold ">{testimonial.name}</p>
+              <p className="text-center text-gray-400 font-poppins font-thin text-sm mb-4">{testimonial.Designation}</p>
+              <p className="text-gray-500 max-w-7xl text-">
                 {index === 1 ? testimonial.text : truncateText(testimonial.text)}
               </p>
               <div className="text-5xl text-dgreen ml-0 lg:ml-56 mt-2">”</div>
